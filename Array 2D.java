d43d5ad68514232b4c150651e0c3c7937ec31316
ishadeshmukh03 <128356387+ishadeshmukh03@users.noreply.github.com>
2023-03-27 07:29:42 -0700

    Add files via upload

---
+++ Array 2D.java
@@ -0,0 +1,35 @@
+    Array2D
+{
+	
+    public static void main(String[] args) 
+	{
+
+	    int matrix[][]={{1,2,3},
+
+	                    {4,5,6},
+	                   
+                            {7,8,9},
+	                   
+                            {10,11,12}
+	                    
+                           };
+		
+            System.out.println("\nt The Matrix is:");
+		
+            for (int i=0;i<4;i++){
+		  
+            for (int j=0;j<3;j++){
+		   
+            System.out.println(""+matrix[i][j]);
+		    
+
+		}
+
+                }
+	       
+            System.out.println();
+	
+            }
+
+    }
+	
